cc0c8f82d2b7f337e800770070e679b5f2893c22
mzivanovic89 <zivanovicmilos11@gmail.com>
2022-12-24 22:00:52 +0100

    removed build directory

--- build/js/types.js
+++
@@ -1,7 +0,0 @@
-export var DIRECTION;
-(function (DIRECTION) {
-    DIRECTION["UP"] = "up";
-    DIRECTION["DOWN"] = "down";
-    DIRECTION["LEFT"] = "left";
-    DIRECTION["RIGHT"] = "right";
-})(DIRECTION || (DIRECTION = {}));
